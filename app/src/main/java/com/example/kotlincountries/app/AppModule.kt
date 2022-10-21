0026c60d72080745e015bfa28a53b6bb3a0fad91
Mirsabuhi Amirov <Mirsabuhi.Amirov@e-manat.az>
2022-10-21 16:19:38 +0400

    add Dagger Hilt

---
+++ app/src/main/java/com/example/kotlincountries/app/AppModule.kt
@@ -0,0 +1,29 @@
+package com.example.kotlincountries.app
+
+import com.example.kotlincountries.service.CountryAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCountryApi() : CountryAPI{
+        return Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(CountryAPI::class.java)
+    }
+}
